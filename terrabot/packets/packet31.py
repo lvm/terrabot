43925758546f55b43773c534686b483c40955280
James Lu <james@overdrivenetworks.com>
2017-07-31 15:18:14 +0800

    packet31: fix wrong variable names (spawnX and spawnY)

--- terrabot/packets/packet31.py
+++ terrabot/packets/packet31.py
@@ -4,5 +4,5 @@ import struct
 class Packet31Parser(object):
 
 	def parse(self, world, player, data, ev_man):
-		player.x = world.spawnx
-		player.y = world.spawny
+		player.x = world.spawnX
+		player.y = world.spawnY
